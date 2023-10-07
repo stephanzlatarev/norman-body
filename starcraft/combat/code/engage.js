3a7b2037554388d6dda53a7c8a86ef5a72131d1a
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-10-07 19:58:36 +0300

    wip

--- starcraft/combat/code/engage.js
+++ starcraft/combat/code/engage.js
@@ -1,65 +1,125 @@
 import Fight from "./fight.js";
 
 export default function(units) {
-  const engagements = [];
+  const fights = [];
   const warriors = [];
   const enemies = [];
 
   for (const unit of units.values()) {
-    if (unit.combat.isWarrior) {
-      warriors.push(unit);
-    } else if (unit.combat.isEnemy) {
+     if (unit.combat.isEnemy) {
+      fights.push(new Fight(unit));
       enemies.push(unit);
+    } else if (unit.combat.isWarrior) {
+      warriors.push(unit);
     }
   }
 
-  if (!warriors.length) return [];
-
-  assessStrength(enemies, warriors);
-
-  enemies.sort((a, b) => (a.combat.strength - b.combat.strength));
-
-  for (const target of enemies) {
-    assessDistance(warriors, target);
-    warriors.sort((a, b) => (a.combat.distance - b.combat.distance));
-
-    const attackers = warriors.slice(0, 2);
-    engagements.push(new Fight(target, attackers));
-
-    warriors.splice(0, 2);
-    if (!warriors.length) break;
+  if (warriors.length && enemies.length) {
+    assessVulnerability(fights, enemies);
+    assessAttacks(fights, warriors);
+    engageClosestWarriors(fights, warriors);
+    optimizeFights(fights, enemies);
+  } else {
+    fights.length = 0;
   }
 
-  return engagements;
+  return fights;
 }
 
-function assessStrength(enemies, warriors) {
-  for (const one of enemies) {
-    one.combat.strength = 0;
+// Assess strength of enemy units based on the health, damage, weapon speed, and distance by movement speed of the unit and its supporting units.
+function assessVulnerability(fights, enemies) {
+  for (const fight of fights) {
+    fight.enemyStrength = 0;
 
-    for (const another of enemies) {
-      if (one !== another) {
-        one.combat.strength += support(one, another);
+    for (const enemy of enemies) {
+      if (fight.enemy !== enemy) {
+        fight.enemyStrength += support(fight.enemy, enemy);
       }
     }
   }
 
-  for (const enemy of enemies) {
-    for (const warrior of warriors) {
-      enemy.combat.strength += distance(enemy, warrior) / 3;
-    }
-  }
-}
-
-function assessDistance(warriors, enemy) {
-  for (const warrior of warriors) {
-    warrior.combat.distance = distance(enemy, warrior);
-  }
+  fights.sort((a, b) => (a.enemyStrength - b.enemyStrength));
 }
 
 function support(a, b) {
   const d = distance(a, b);
   return (d < 10) ? (10 - d) : 0;
+}
+
+// Assess attacks of a warrior in a fight by warrior's damage, weapon speed, time to load weapon, and distance by movement speed avoiding obstacles.
+function assessAttacks(fights, warriors) {
+  for (const fight of fights) {
+    for (const warrior of warriors) {
+      fight.addAttack(warrior, distance(warrior, fight.enemy));
+    }
+  }
+}
+
+// Match warriors with the closest enemy unit.
+function engageClosestWarriors(fights, warriors) {
+  for (const warrior of warriors) {
+    let selectedFight;
+    let selectedDistance = Infinity;
+
+    for (const fight of fights) {
+      // TODO: Use smarter way to choose which warrior to enage first with an enemy
+      if (isEnemyOverPowered(fight)) continue;
+
+      const distance = fight.attacks.get(warrior);
+
+      if (distance < selectedDistance) {
+        selectedFight = fight;
+        selectedDistance = distance;
+      }
+    }
+
+    selectedFight.engageWarrior(warrior);
+  }
+}
+
+// Make sure the most vulnerable enemies are engaged with priority, and that all enemies are outpowered.
+function optimizeFights(fights) {
+  for (let i = 0; i < fights.length; i++) {
+    const fight = fights[i];
+
+    if (!isEnemyOverPowered(fight)) {
+      // Find warriors from later fights that can over-power the enemy in this fight
+      const supports = [];
+
+      for (let j = i + 1; j < fights.length; j++) {
+        const lessImportantFight = fights[j];
+
+        for (const warrior of lessImportantFight.warriors) {
+          if (areAttacksComparable(lessImportantFight.getAttack(warrior), fight.getAttack(warrior))) {
+            supports.push({ warrior: warrior, fight: lessImportantFight });
+          }
+        }
+      }
+
+      if (isEnemyOverPowered(fight, supports.map(one => one.warrior))) {
+        // The supporting warriors will over-power the enemy
+        for (const support of supports) {
+          fight.engageWarrior(support.warrior);
+          support.fight.disengageWarrior(support.warrior);
+
+          if (isEnemyOverPowered(fight)) break;
+        }
+      }
+    }
+  }
+}
+
+// An enemy is over-powered when the attacking warriors can kill it before it kills them
+function isEnemyOverPowered(fight, ...supportWarriors) {
+  const warriorsCount = fight.warriors.size + (supportWarriors ? supportWarriors.length : 0);
+
+  return (warriorsCount >= 2);
+}
+
+// A second attack is comparable to the first attack if its wasted damage is not very much
+function areAttacksComparable(first, second) {
+  // Currently the attack is simply the distance between the warrior and enemy
+  return (second - first < 1);
 }
 
 function distance(a, b) {
--- starcraft/combat/code/fight.js
+++ starcraft/combat/code/fight.js
@@ -1,9 +1,29 @@
 
 export default class Fight {
 
-  constructor(enemy, warriors) {
+  constructor(enemy) {
     this.enemy = enemy;
-    this.warriors = warriors;
+    this.enemyStrength = 0;
+    this.attacks = new Map();
+    this.warriors = new Set();
+  }
+
+  getAttack(warrior) {
+    return this.attacks.get(warrior);
+  }
+
+  addAttack(warrior, attack) {
+    this.attacks.set(warrior, attack);
+  }
+
+  engageWarrior(warrior) {
+console.log(warrior.tag, "engages", this.enemy.tag, "distance:", this.attacks.get(warrior));
+    this.warriors.add(warrior);
+  }
+
+  disengageWarrior(warrior) {
+console.log(warrior.tag, "disengages", this.enemy.tag, "distance:", this.attacks.get(warrior));
+    this.warriors.delete(warrior);
   }
 
 }
