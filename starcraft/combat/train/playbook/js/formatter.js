89ac9249be20eff4ceebbce4d60f124368017bcd
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-12-08 11:09:48 +0200

    Try playbook with more parameters and less strategies

--- starcraft/combat/train/playbook/js/formatter.js
+++ starcraft/combat/train/playbook/js/formatter.js
@@ -1,6 +1,6 @@
 
-const WARRIORS_SLOTS = 3;
-const ENEMIES_SLOTS = 4;
+const WARRIORS_SLOTS = 10;
+const ENEMIES_SLOTS = 10;
 
 export default function(sample, mapping) {
   const warrior = sample.list(mapping.Warrior)[0];
@@ -12,23 +12,41 @@ export default function(sample, mapping) {
   const output = [target.properties.x, target.properties.y];
 
   // Add this warrior
-  input.push(0, 0, warrior.properties.health);
+  input.push(warrior.properties.radius);
+  input.push(warrior.properties.speed);
+  input.push(warrior.properties.health);
+  input.push(warrior.properties.damage);
+  input.push(warrior.properties.range);
+  input.push(warrior.properties.cooldown);
 
   // Add support warriors
   for (const one of warriors) {
     if (one) {
-      input.push(one.properties.x, one.properties.y, one.properties.health);
+      input.push(one.properties.x);
+      input.push(one.properties.y);
+      input.push(one.properties.radius);
+      input.push(one.properties.speed);
+      input.push(one.properties.health);
+      input.push(one.properties.damage);
+      input.push(one.properties.range);
+      input.push(one.properties.cooldown);
     } else {
-      input.push(0, 0, 0);
+      input.push(0, 0, 0, 0, 0, 0, 0, 0);
     }
   }
 
   // Add enemies
   for (const one of enemies) {
     if (one) {
-      input.push(one.properties.x, one.properties.y, one.properties.health);
+      input.push(one.properties.x);
+      input.push(one.properties.y);
+      input.push(one.properties.radius);
+      input.push(one.properties.speed);
+      input.push(one.properties.health);
+      input.push(one.properties.damage);
+      input.push(one.properties.range);
     } else {
-      input.push(0, 0, 0);
+      input.push(0, 0, 0, 0, 0, 0, 0);
     }
   }
 
